5e3c736a3d98e9f9ace26e61a91a2c7c1fab095f
glyzamae06 <Glyzamaeloyola@gmail.com>
2026-02-09 00:05:20 +0800

    contact

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 const openBtn = document.getElementById("menu-open-button");
 const closeBtn = document.getElementById("menu-close-button");
+const navLinks = document.querySelectorAll(".nav-link");
 
-openBtn.addEventListener("click", ()=>{
+openBtn.addEventListener("click", () => {
   document.body.classList.add("show-mobile-menu");
 });
 
-closeBtn.addEventListener("click", ()=>{
+closeBtn.addEventListener("click", () => {
   document.body.classList.remove("show-mobile-menu");
+});
+
+/* close when clicking any link */
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+  });
 });
